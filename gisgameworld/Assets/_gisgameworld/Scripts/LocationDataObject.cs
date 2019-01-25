63d6c9838d3bda7605bab72d3628485219eb0bb9
Ryan Dieno <ryan.dieno@gmail.com>
2019-01-24 19:55:58 -0800

    Created initial project and obtaining device gps coords

---
+++ gisgameworld/Assets/_gisgameworld/Scripts/LocationDataObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Coordinate
+{
+    public float latitude;
+    public float longitude;
+}
+
+[CreateAssetMenu(fileName = "LocationData", menuName = "Custom/LocationData", order = 1)]
+public class LocationDataObject : ScriptableObject
+{
+    public Coordinate[] coords;
+}
