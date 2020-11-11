1748ca0bf65555a66a37d6e0c3bf503e2e175993
Ryan Dieno <ryan.dieno@gmail.com>
2020-11-11 14:51:24 -0800

    fixed issue where game manager parent class was removed

--- gisgameworld/Assets/_gisgameworld/Scripts/GameManager.cs
+++ gisgameworld/Assets/_gisgameworld/Scripts/GameManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager
+public class GameManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject level = null;
